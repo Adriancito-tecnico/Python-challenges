3276381bed5cd629e5b898d5232eef2c93d72037
Adriancito-tecnico <adrian1101.0110.1100@gmail.com>
2026-08-08 23:34:45 +0200

    Delete Temporada 1| Season 1/Resolviendo problemas de programación en python - P1 - Observador de actualizaciones en variables.py

--- Temporada 1| Season 1/Resolviendo problemas de programación en python - P1 - Observador de actualizaciones en variables.py
+++
@@ -1,8 +0,0 @@
-a = "1"
-X = a
-while True:
-    a = input("Introduzca el valor de a:")
-    if a != X:
-        print("La variable a ha sido actualizada")
-        print(f"Valor antiguo: {X} Valor nuevo: {a}")
-        X = a
